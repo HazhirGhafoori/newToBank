f813628bec492b15dfa920eaae11981197efa8c2
HazhirGhafoori <HazhirGhafoori@gmail.com>
2022-12-29 10:42:48 +0330

    add new changes to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'yekan': ['Vazirmatn'],
+    },
+    extend: {
+
+
+    },
   },
   plugins: [],
 }
